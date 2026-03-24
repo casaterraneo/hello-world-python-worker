50f7690e84801b0013e37b6701668a7107218518
Paolo Terraneo <63970391+casaterraneo@users.noreply.github.com>
2026-03-24 18:15:45 +0100

    Update response message in fetch method to include 'TEST'

--- src/entry.py
+++ src/entry.py
@@ -3,4 +3,4 @@ from workers import WorkerEntrypoint, Response
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        return Response("Hello world!")
+        return Response("Hello world TEST!")
