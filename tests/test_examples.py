56c7c3806e97f74f35ae4ac5655bae9e2abdcc91
Paolo Terraneo <63970391+casaterraneo@users.noreply.github.com>
2026-03-24 17:38:58 +0100

    Add test fixtures and example test for pywrangler dev server

---
+++ tests/test_examples.py
@@ -0,0 +1,12 @@
+import requests
+import pytest
+import subprocess
+from conftest import REPO_ROOT
+
+
+def test_01_hello(dev_server):
+    port = dev_server
+    response = requests.get(f"http://localhost:{port}")
+    assert response.status_code == 200
+    assert response.text == "Hello world!"
+    assert response.headers["content-type"] == "text/plain;charset=UTF-8"
