6cb5dc06866d2bc8b925d3207d82a9348df86430
Paolo Terraneo <63970391+casaterraneo@users.noreply.github.com>
2026-03-27 10:53:27 +0100

    fix: update fetch method to use KV_BINDING instead of FOO for data storage

--- src/entry.py
+++ src/entry.py
@@ -3,6 +3,6 @@ from workers import WorkerEntrypoint, Response
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        await self.env.FOO.put("bar", "baz")
-        bar = await self.env.FOO.get("bar")
+        await self.env.KV_BINDING.put("bar", "baz")
+        bar = await self.env.KV_BINDING.get("bar")
         return Response(f"Hello world TEST! Version: {self.env.APP_VERSION} {bar}")
