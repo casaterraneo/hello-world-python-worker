acffdf49fa0e9ee35ad6789d71fc5f44b38a5066
Paolo Terraneo <63970391+casaterraneo@users.noreply.github.com>
2026-03-27 11:06:07 +0100

    feat: refactor root endpoint and move KV_BINDING logic to FastAPI app

--- src/entry.py
+++ src/entry.py
@@ -1,8 +1,17 @@
+from fastapi import FastAPI, Request
 from workers import WorkerEntrypoint, Response
 
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    await self.env.KV_BINDING.put("bar", "baz")
+    bar = await self.env.KV_BINDING.get("bar")
+    # return Response(f"Hello world TEST! Version: {self.env.APP_VERSION} {bar}")
+    message = f"Hello world TEST! Version: {self.env.APP_VERSION} {bar}"
+    return {message}
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        await self.env.KV_BINDING.put("bar", "baz")
-        bar = await self.env.KV_BINDING.get("bar")
-        return Response(f"Hello world TEST! Version: {self.env.APP_VERSION} {bar}")
+        import asgi
+        return await asgi.fetch(app, request.js_object, self.env)
